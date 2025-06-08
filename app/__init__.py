2d2f0d3c7289abaf4cf9acdad9cc1690f992a7f4
YasserDeveloper89 <yass.gonzaleszea@gmail.com>
2025-06-09 00:12:06 +0200

    Delete app directory

--- app/__init__.py
+++
@@ -1,2 +0,0 @@
-# app/__init__.py
-# Inicialización del paquete app
--- app/utils.py
+++
@@ -1,8 +0,0 @@
-def format_currency(value, currency="S/"):
-    """
-    Formatea número como moneda local, con símbolo y dos decimales.
-    """
-    try:
-        return f"{currency} {value:,.2f}"
-    except:
-        return value
